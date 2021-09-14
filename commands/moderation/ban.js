a4c43619e2489b017cb4a4698d678e4d0a60c6ec
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-09-14 17:34:27 +0500

    improved ban.js
    The cmd actually bans now and DMs the banned user (if they have DMs on).

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -3,6 +3,7 @@ const { MessageEmbed } = require("discord.js")
 module.exports = {
     name: 'ban',
     description: 'bans the user',
+    category: 'moderation',
     permissions: 'BAN_MEMBERS',
     async run(client, message, args) {
         //the member that the user is trying to ban
@@ -11,14 +12,30 @@ module.exports = {
         //if the target is not found
         if (!target || !args[0]) return message.channel.send('Please provide a valid user to ban')
         
+        if (target.id === message.author.id) return message.channel.send('I am afraid that you cannot ban yourself..')
+
+        if (target.id === client.user.id) return message.channel.send('You\'re gonna ban me? **I am unbannable...**')
+
         const reason = args.slice(1).join(' ') || 'No reason specified'
 
         const banEmbed = new MessageEmbed()
             .setTitle('User Banned!')
-            .addField('Banned User', target.user.tag)
+            .addField('Banned User', `target.user.tag (${target.id})`)
             .addField('Reason', reason)
+            .setColor(0xFFFF00)
         
+        const banUserEmbed = new MessageEmbed()
+            .setTitle('Banned!')
+            .setDescription(`You have been banned from ${message.guild.name}!`)
+            .addField('Reason', reason)
+            .setColor(0xFFFF00)
 
-        message.channel.send(`<@${target.id}> has been banned for ${reason}`)
+        
+        target.send({ embeds: [banUserEmbed]}).catch(err => {
+            console.log(err)
+        })
+        
+        target.ban({ reason })
+            .then(() => message.channel.send({ embeds: [banEmbed] }))
     }
 }
