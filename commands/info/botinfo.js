bbffad93cacd06cb8dc7965a649f043c30cadd07
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-09-15 08:14:50 +0500

    added a >botinfo command!

---
+++ commands/info/botinfo.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: 'botinfo',
+    description: 'Sends info of FadBot',
+    category: 'moderation',
+    run (client, message, args) {
+        const embed = new MessageEmbed()
+            .setTitle('FadBot Info')
+            .setDescription('FadBot is a multi-purpose discord bot currently being developed. Its set to have some moderation commands; fun commands; commands that use the new API features like buttons, dropdowns and etc; and many other features along the way! ')
+            .addField('Devs', 'Fad The Chad (DankML)#8516')
+            .addField('Version', 'v0.2.0')
+            .addField('Github', '[Leave us a star for support!](https://github.com/FadTheChad/FadBot)')
+            .addField('Support Server', '[Join our Official Support Server!](https://discord.gg/3tEGymY5pE)')
+            .setColor(0xFFFF00)
+            .setThumbnail(client.user.avatarURL({dynamic: true}))
+
+        message.channel.send({embeds: [embed]})
+    }
+}
