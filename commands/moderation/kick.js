2f7ff5937b406fe5b62c46edc9759e1806e5270b
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-09-16 23:07:34 +0500

    added >kick!

---
+++ commands/moderation/kick.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: 'kick',
+    description: 'kicks the user',
+    usage: '<member> [reason]',
+    category: 'moderation',
+    permissions: 'KICK_MEMBERS',
+    async run (client, message, args) {
+        //the member that the user is trying to kick
+        const target = message.mentions.members.first() || await message.guild.members.fetch(args[0])
+        
+        //if the target is not found
+        if (!target || !args[0]) return message.channel.send('Please provide a valid user to kick')
+        
+        if (target.id === message.author.id) return message.channel.send('I am afraid that you cannot kick yourself..')
+
+        if (target.id === client.user.id) return message.channel.send('You\'re gonna kick me? **I am unkickable...**')
+
+        const reason = args.slice(1).join(' ') || 'No reason specified'
+
+        const kickEmbed = new MessageEmbed()
+            .setTitle('User Kicked!')
+            .addField('Kicked User', `target.user.tag (${target.id})`)
+            .addField('Reason', reason)
+            .setColor(0xFFFF00)
+        
+        const kickUserEmbed = new MessageEmbed()
+            .setTitle('Kicked!')
+            .setDescription(`You have been kicked from ${message.guild.name}!`)
+            .addField('Reason', reason)
+            .setColor(0xFFFF00)
+
+        
+        target.send({ embeds: [kickUserEmbed]}).catch(err => {
+            console.log(err)
+        })
+        
+        target.kick({ reason })
+            .then(() => message.channel.send({ embeds: [kickEmbed] }))
+    }
+}
