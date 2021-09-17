e556f7f6fe73ac7a3b9ff5f97eeb5123efa0f156
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-09-17 14:36:22 +0500

    Added a `>say` command!

---
+++ commands/misc/say.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: 'say',
+    description: 'Makes the bot say the specified message',
+    aliases: ['repeat'],
+    usage: '<message>',
+    category: 'misc',
+    permissions: 'ADMINISTRATOR', // keeping this closed to admins because this can be heavily abused, so uh, yeah.
+    run (client, message, args) {
+        const said = args.join(' ')
+
+        if (!said || !args[0]) {
+            const errEmbed = new MessageEmbed()
+                .setTitle('<:FadBot_Cross:887607566060888094> Message Not Specified!')
+                .setDescription('Please specify a message!')
+                .setColor(0x0000FF)
+
+            return message.channel.send({embeds: [errEmbed]})
+        }
+
+        message.delete() //deletes the original message
+        
+        message.channel.send(said)
+    }
+}
