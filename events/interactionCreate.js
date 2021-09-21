d0697af7800967645911c096706e4c65dab625e6
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-09-21 17:49:07 +0500

    Added a perms check in slash cmds!

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,3 +1,6 @@
+const { devs } = require('../config.json')
+const { fbEmbed } = require('../utils/fbEmbed-utils')
+
 module.exports = {
     name: 'interactionCreate',
     async run (interaction, client) {
@@ -6,10 +9,41 @@ module.exports = {
 
             if (!slashCommand) return 
 
+            if (slashCommand.permissions) {
+                let { permissions } = slashCommand
+                    
+                if ((permissions === 'BOT_DEV' || slashCommand.category === 'dev') && !devs.includes(interaction.user.id)) return
+                
+                if (typeof permissions === 'string') permissions = [permissions]
+            
+                for (const permission of permissions)  {
+                    const permErrEmbed = fbEmbed('error', 'Invalid Permissions!')
+                    
+                    if (!interaction.member.permissions.has(permission)) {
+                        permErrEmbed.setDescription('You don\'t have perms to run this command!')
+                        return interaction.reply({ embeds: [permErrEmbed], ephemeral: true })
+                    }
+                    
+                    if (!interaction.guild.me.permissions.has(permission)) {
+                        permErrEmbed.setDescription('I don\'t have perms to run this command!')
+                        return interaction.reply({ embeds: [permErrEmbed], ephemeral: true })
+                    }
+                }
+            
+            }
+
             try {
                 await slashCommand.slashRun(client, interaction)
             } catch (err) {
                 console.error(err)
+
+                const errEmbed = fbEmbed(
+                    'error',
+                    'Error!', 
+                    'Hey you!\nYeah you!\nif you\'re seeing this message, it means that the bot owner did a stinky in writing the code thus you receiving an error after trying to run a commound. sorry bout that.'
+                )
+                    
+                interaction.reply({ embeds: [errEmbed], ephemeral: true })
             }
         }
     }
