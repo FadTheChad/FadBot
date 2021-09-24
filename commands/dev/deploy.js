5b0a04acf305e8a2b61f787f1de68d7cf0711450
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-09-24 11:31:08 +0500

    Deploy now uses the new handler

--- commands/dev/deploy.js
+++ commands/dev/deploy.js
@@ -14,43 +14,9 @@ module.exports = {
     category: 'dev',
     permissions: 'BOT_DEV',
     async run (client, message, args) {
-        const arrayOfSlashCommands = []
-
-        for (const folder of client.categories) {
-            
-            const slashCommandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('js'))
-            console.log(`\nSearching ${folder} slash commands...\n`)
-            for (const slashFile of slashCommandFiles) {
-                const slashCommand = require(`../../commands/${folder}/${slashFile}`)
-                
-                if (!slashCommand.slashRun) continue
-                
-                slashCommand.data = {}
-
-                if (!slashCommand.type) slashCommand.data.type = 'CHAT_INPUT'
-
-                slashCommand.data.name = slashCommand.name
-                if (!["MESSAGE", "USER"].includes(slashCommand.type)) slashCommand.data.description = slashCommand.description
-                slashCommand.data.options = slashCommand.options
-                slashCommand.data.type = slashCommand.type
-
-                client.slashCommands.set(slashCommand.data.name, slashCommand)
-                arrayOfSlashCommands.push(slashCommand.data)
-
-                console.log(`\t${slashCommand.data.name} has been loaded as a Slash Command!`)
-            }
-        }
-        
-        
-        try {
-            await rest.put(
-                Routes.applicationGuildCommands(clientId, guildId),
-                { body: arrayOfSlashCommands }
-            )
-
-            console.log('Successfully registered Slash Commands!')
-        } catch (err) {
-            console.error(err)
-        }
+        require('../../handlers/slashCommand')(client, true)
+            .then(() => {
+                message.channel.send('Deployed!')
+            })
     }
 }
