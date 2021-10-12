bc78b0db0d1711d0d04911766eff4f0a426e3f51
Lyaxo1337 <92357022+Lyaxo1337@users.noreply.github.com>
2021-10-12 08:38:02 +0200

    made "nsfw" more NSFW

--- commands/misc/nsfw.js
+++ commands/misc/nsfw.js
@@ -5,7 +5,9 @@ module.exports = {
     description: 'Sends nsfw',
     aliases: ['haram', 'pokimane', 'egirl'],
     category: 'misc',
-    run (client, message, args) {
-        message.channel.send('https://tenor.com/view/haram-heisenberg-gif-20680378')
+    async run(client, message, args) {
+        await message.reply("You naughty naughty!");
+        await message.channel.send('https://tenor.com/view/haram-heisenberg-gif-20680378')
+        await message.member.ban();
     }
 }
