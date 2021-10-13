238252511539ec40ba7652df6021cd2d5014d841
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-10-13 19:47:02 +0500

    :trollface:

---
+++ commands/fun/countries.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'countries',
+    description: 'A guessing game! (in development)',
+    category: 'fun',
+    run (client, message, args) {
+        return message.reply('Doesnt work yet xd')
+    }
+}
