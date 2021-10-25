5276bb85e4255e62f62ff7fb45c0264b2dd81c8e
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-10-25 17:56:28 +0500

    testing dbCache with prefix module

--- commands/utility/prefix.ts
+++ commands/utility/prefix.ts
@@ -12,7 +12,7 @@ const command: ICommand = {
     async run (client, message, args) {
         let newPrefix = args[0]
 
-        if (newPrefix.toLowerCase() === '-default') newPrefix = '>'
+        if (newPrefix?.toLowerCase() === '-default') newPrefix = '>'
 
         if (!newPrefix || newPrefix.length > 3) {
             const errEmbed = fbEmbed('error', 'Invalid Prefix!', 'Please specify a valid prefix of 1-3 characters!')
@@ -20,7 +20,7 @@ const command: ICommand = {
             return message.channel.send({ embeds: [errEmbed] })
         }
 
-        setPrefix(message.guild!.id, newPrefix)
+        setPrefix(message.guild!.id, newPrefix, client)
 
         message.channel.send('Works maybe!')
     }
--- utils/db/prefix-utils.ts
+++ utils/db/prefix-utils.ts
@@ -3,10 +3,11 @@ import { Snowflake } from 'discord.js'
 import { Document } from 'mongoose'
 import IGuild from '../../structure/interfaces/db/IGuild'
 import { prefix as defaultPrefix } from '../../config.json'
+import FadBotClient from '../../structure/Client'
 
 let prefixCache: {[key: string]: string | undefined} = {}
 
-export const setPrefix = (guildId: Snowflake, newPrefix: string) => {
+export const setPrefix = (guildId: Snowflake, newPrefix: string, client: FadBotClient) => {
     Schema.findOne({_id: guildId}, async (err: Error, data: Document<any, any, IGuild> & IGuild & { _id: string }) => {
         if (data) {
             data.config.prefix = newPrefix
@@ -23,18 +24,22 @@ export const setPrefix = (guildId: Snowflake, newPrefix: string) => {
             data.save()
         }
 
-        prefixCache[guildId] = newPrefix
+        if (!client.dbCache.guilds[guildId]) client.dbCache.guilds[guildId] = {}
+
+        client.dbCache.guilds[guildId]!.prefix = newPrefix
     })
 }
 
-export const getPrefix = async (guildId: Snowflake) => {
-    const cachedPrefix = prefixCache[guildId]
+export const getPrefix = async (guildId: Snowflake, client: FadBotClient) => {
+    const cachedPrefix = client.dbCache.guilds[guildId]?.prefix
 
     if (cachedPrefix) return cachedPrefix
 
     const result = await Schema.findOne({ _id: guildId })
 
-    prefixCache[guildId] = result?.config.prefix || defaultPrefix
+    if (!client.dbCache.guilds[guildId]) client.dbCache.guilds[guildId] = {}
+
+    client.dbCache.guilds[guildId]!.prefix = result?.config.prefix || defaultPrefix
 
     return result?.config.prefix
 }
