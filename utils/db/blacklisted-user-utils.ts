d1e8f8f38cfd25663e2301f0691ba7d4c2ad0c22
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-11-06 00:13:56 +0500

    User blacklisted utils now uses dbCache!

--- utils/db/blacklisted-user-utils.ts
+++ utils/db/blacklisted-user-utils.ts
@@ -2,8 +2,9 @@ import Schema from '../../schemas/User'
 import { Snowflake } from 'discord.js'
 import { Document } from 'mongoose'
 import IUser from '../../structure/interfaces/db/IUser'
+import FadBotClient from '../../structure/Client'
 
-export const setBlacklisted = (userId: Snowflake, bool: boolean) => {
+export const setBlacklisted = (userId: Snowflake, bool: boolean, client: FadBotClient) => {
     Schema.findOne({_id: userId}, async (err: Error, data: Document<any, any, IUser> & IUser & { _id: string }) => {
         if (err) throw err
 
@@ -19,19 +20,37 @@ export const setBlacklisted = (userId: Snowflake, bool: boolean) => {
 
             data.save()
         }
+
+        client.validateDbCache(client.dbCache.users, userId)
+
+        client.dbCache.users.get(userId)!.blacklisted = bool
     })
 }
 
-export const isBlacklisted = async (userId: Snowflake): Promise<boolean> => {
+export const isBlacklisted = async (userId: Snowflake, client: FadBotClient): Promise<boolean> => {
+    let cachedValue = client.dbCache.users.get(userId)?.blacklisted
+
+    if (cachedValue != undefined) return cachedValue
+
     const result = await Schema.findOne({ _id: userId })
+
+    client.validateDbCache(client.dbCache.users, userId)
+
+    client.dbCache.users.get(userId)!.blacklisted = result?.blacklisted ?? false
 
     if (!result) return false
 
     return result?.blacklisted
 }
 
-export const getBList = async () => {
+export const getBList = async (client: FadBotClient): Promise<Snowflake[]> => {
+    let cachedList = [...client.dbCache.users.keys()].filter(key => {
+        return client.dbCache.users.get(key)!.blacklisted
+    })
+
+    if (cachedList.length) return cachedList
+
     let list = await Schema.find({ blacklisted: true })
 
-    return list.map(user => user.id)
+    return list?.map(user => user.id) || []
 }
