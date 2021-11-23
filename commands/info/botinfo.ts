e555e10782cc9a525674c150316cf2520cf7674a
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-11-23 22:33:21 +0500

    bot info now shows version from package.json

--- commands/info/botinfo.ts
+++ commands/info/botinfo.ts
@@ -1,5 +1,6 @@
 import fbEmbed from '../../utils/fbEmbed-utils'
 import ICommand from '../../structure/interfaces/ICommand'
+import { version } from '../../package.json'
 
 const command: ICommand = {
     name: 'botinfo',
@@ -13,7 +14,7 @@ const command: ICommand = {
             'FadBot is a multi-purpose discord bot currently being developed. Its set to have some moderation commands; fun commands; commands that use the new API features like buttons, dropdowns and etc; and many other features along the way!'
         )
             .addField('Devs', 'Fad The Chad (DankML)#8516')
-            .addField('Version', 'v1.7.0')
+            .addField('Version', version as string)
             .addField('Github', '[Leave us a star for support!](https://github.com/FadTheChad/FadBot)', true)
             .addField('Support Server', '[Join our Official Support Server!](https://discord.gg/3tEGymY5pE)', true)
             .addField('Trello', '[Get A Look At Upcoming Features and Ideas!](https://trello.com/b/4qiwoazB/fadbot-board)', true)
