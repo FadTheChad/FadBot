2fd6bf4011e0e62037b2231040ced70fc8e4ed03
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-11-24 23:59:22 +0500

    displayResult now shows event table aswell

--- structure/client-utils/displayResult.ts
+++ structure/client-utils/displayResult.ts
@@ -3,6 +3,7 @@ import { table } from 'table'
 import chalk from 'chalk'
 
 const displayResult = (client: FadBotClient) => {
+    // Command Table
     let data = [
         [chalk.green.underline('No'), chalk.yellow.underline('Name'), chalk.rgb(255, 255, 255).underline('Command Count')],
     ]
@@ -26,6 +27,34 @@ const displayResult = (client: FadBotClient) => {
     console.log(table(data, {
         header: {
             content: chalk.rgb(250, 169, 70)('Command Categories'),
+            alignment: 'center',
+        },
+        columns: {
+            0: { alignment: 'center' },
+            2: { alignment: 'center' }
+        }
+    }))
+
+    // Event Table
+    let data2 = [
+        [chalk.green.underline('No'), chalk.yellow.underline('Name'), chalk.rgb(255, 255, 255).underline('Once')],
+    ]
+
+    let i2 = 0
+
+    for (let [eventName, eventData] of client.events) {
+        data2.push([
+            chalk.green(i2.toString()),
+            chalk.yellow(eventName),
+            chalk.rgb(255, 255, 255)((eventData.once ?? false).toString())
+        ])
+
+        i++
+    }
+
+    console.log(table(data2, {
+        header: {
+            content: chalk.rgb(5, 247, 82)('Events'),
             alignment: 'center',
         },
         columns: {
