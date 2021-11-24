fb9319f4df6eab9ef48eca779af8978ef0e19273
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-11-25 00:00:34 +0500

    removed test logs and changed once bool to true

--- events/ready.ts
+++ events/ready.ts
@@ -2,11 +2,9 @@ import IEvent from '../structure/interfaces/IEvent'
 
 const event: IEvent = {
     name: 'ready',
+    once: true,
     run (client) {
         client.fbLogger.log('Ready', `${client.user?.username} is ready!\n`)
-
-        client.fbLogger.warn('This is a test warn')
-        client.fbLogger.error('This is a test error', 'Ready')
     }
 }
 
