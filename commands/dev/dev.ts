015f6750524cf8455841694a499d56fa337b5b44
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-12-09 18:22:53 +0500

    Testing method

--- commands/dev/dev.ts
+++ commands/dev/dev.ts
@@ -1,4 +1,6 @@
 import ICommand from '../../structure/interfaces/ICommand'
+import { FBEmbed } from '../../utils/fbEmbed-utils'
+import wait from '../../utils/wait'
 
 const command: ICommand = {
     name: 'dev',
@@ -8,6 +10,31 @@ const command: ICommand = {
     permissions: 'BOT_DEV',
     run (client, message, args) {
         message.reply('is dev')
+
+        let embed = new FBEmbed().setBase('success', 'Testing')
+
+        embed.sendLoadingMsgAndEditConstantly(message.channel, [{
+            loadMessage: 'Trying to test time thing...',
+            callback: async () => {
+                return args[0] !== 'err';
+            },
+            onResolveMsg: 'Done!',
+            onRejectMsg: 'Uh oh stinky'
+        }, {
+            loadMessage: 'Trying to test time thing...',
+            callback: async () => {
+                return args[0] !== 'err';
+            },
+            onResolveMsg: 'Done!',
+            onRejectMsg: 'Uh oh stinky'
+        }, {
+            loadMessage: 'Trying to test time thing...',
+            callback: async () => {
+                return false
+            },
+            onResolveMsg: 'Done!',
+            onRejectMsg: 'Uh oh stinky'
+        }], 2500)
     },
     async slashRun (client, interaction) {
         interaction.reply('is dev')
