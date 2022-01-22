1ea6cac03d8227fa95cd69f52052c8c95b3c813f
Fad F <fmzfads@gmail.com>
2022-01-22 23:21:52 +0500

    War is Peace, Freedom is Slavery, Ignorance is Strength

--- utils/db/blacklisted-word-utils.ts
+++ utils/db/blacklisted-word-utils.ts
@@ -3,11 +3,56 @@ import Schema from '../../schemas/Guild'
 import { Snowflake } from 'discord.js'
 import { Document } from 'mongoose'
 import IGuild from '../../structure/interfaces/db/IGuild'
+import guild from '../../schemas/Guild'
 
 // Gonna be real honest
 // I have no fucking idea if this works
 // i just wrote this in the middle of a nap so bad code incoming
 // (not that i write good code when i am NOT in the mid of a nap kekw)
+
+export const hasBlacklistedWord = async (guildId: Snowflake, content: string, client: FadBotClient) => {
+    const getPossibilities = (word: string, pnc: string) => {
+        let vowels = ['a', 'e', 'i', 'o', 'u']
+
+        let positions = []
+
+        for (let i = 0; i < word.length; i++) {
+            if (vowels.indexOf(word[i]) !== -1) positions.push(i)
+        }
+
+        let result: string[] = [], count = 2 ** positions.length;
+
+        for (let i = 0; i < count; i++) {
+            let newWord = [...word]
+
+            for (let j = 0; j < positions.length; j++) {
+                if ((i >> j) & 1) newWord[positions[j]] = pnc
+            }
+
+            result.push(newWord.join(''))
+        }
+        return result
+    }
+
+    let blAlts = (await getBlacklistedWords(guildId, client)).map(blElement => {
+        return { word: blElement.word, level: blElement.level, possibilities: [...new Set(['*', '#', '$'].map(p => getPossibilities(blElement.word, p)).flat(1))] }
+    })
+
+    let found = blAlts.filter(element => {
+        const foundPossibility = element.possibilities.filter(p => {
+            return content.toLowerCase().split(/ +/g).includes(p)
+        })
+
+        if (foundPossibility.length) return element
+    })
+
+    for (let lvl of [2, 1, 0]) {
+        let lvlWord = found.find(element => element.level === lvl)
+
+        if (lvlWord) return lvlWord
+    }
+}
+
 export const addBlacklistedWord = (guildId: Snowflake, word: string, level: number, client: FadBotClient) => {
     Schema.findOne({ _id: guildId}, (err: Error, data: Document<any, any, IGuild> & IGuild & { _id: string; }) => {
         if (err) throw err
